d627d6499abfb78208258acca832a897afb20b7f
bipin <kumarbipin247@gmail.com>
2024-12-29 11:41:59 +0530

    Code

--- rg.tf
+++ rg.tf
@@ -1,13 +1,13 @@
 resource "azurerm_resource_group" "rgnew" {
-  name = "bk-rg5"
-location = "westeurope"
+  name     = "bk-rg5"
+  location = "westeurope"
 }
 
 resource "azurerm_resource_group" "rgsnew1" {
-  name = "bk-rg6"
-location = "westeurope"
+  name     = "bk-rg6"
+  location = "westeurope"
 }
 resource "azurerm_resource_group" "rgnew2" {
-  name = "bk-rg10"
-location = "westeurope"
+  name     = "bk-rg10"
+  location = "westeurope"
 }
