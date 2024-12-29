8177e0b2a65764943918123435647bc2e072a1d8
bipin <kumarbipin247@gmail.com>
2024-12-29 12:31:31 +0530

    New

--- rg.tf
+++ rg.tf
@@ -2,12 +2,3 @@ resource "azurerm_resource_group" "rgnew" {
   name     = "bk-rg5"
   location = "westeurope"
 }
-
-resource "azurerm_resource_group" "rgsnew1" {
-  name     = "bk-rg6"
-  location = "westeurope"
-}
-resource "azurerm_resource_group" "rgnew2" {
-  name     = "bk-rg10"
-  location = "westeurope"
-}
